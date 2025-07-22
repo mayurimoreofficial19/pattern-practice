ecc0d9781a9e7dfb8862fe659bfb1233bac9acc0
mayurimoreofficial19 <mayurimoreofficial19@gmail.com>
2025-07-21 21:43:39 -0500

    Second commit:hollow left triangle

---
+++ hollow-left-triangle.jsx
@@ -0,0 +1,18 @@
+let num = 5;
+let pattern = "";
+
+if (num > 1) {
+  for (let i = 1; i <= num; i++) {
+    for (let j = 1; j <= i; j++) {
+      if (j == 1 || j == i || i == num) {
+        pattern += "* ";
+      } else {
+        pattern += "  ";
+      }
+    }
+    pattern += "\n";
+  }
+  console.log(pattern);
+} else {
+  pattern = "Number must be greater tha 1 to print start pattern";
+}

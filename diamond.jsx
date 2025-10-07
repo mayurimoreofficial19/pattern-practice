bb1c93d8aba0c8d09dd3b51f20926cd4143111c3
mayurimoreofficial19 <mayurimoreofficial19@gmail.com>
2025-10-06 19:52:01 -0500

    diamond pattern

---
+++ diamond.jsx
@@ -0,0 +1,26 @@
+let pattern = "";
+let num = 5;
+
+for (let i = 1; i <= num; i++) {
+  for (let j = 1; j <= num - i; j++) {
+    pattern += "  ";
+  }
+
+  for (let k = 1; k <= 2 * i - 1; k++) {
+    pattern += "* ";
+  }
+  pattern += "\n";
+}
+
+for (let i = num - 1; i >= 1; i--) {
+  for (let j = 1; j <= num - i; j++) {
+    pattern += "  ";
+  }
+
+  for (let k = 1; k <= 2 * i - 1; k++) {
+    pattern += "* ";
+  }
+  pattern += "\n";
+}
+
+console.log(pattern);

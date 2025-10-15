4d89b651fedbc5e37281cf65b4681ae8e08c46d6
mayurimoreofficial19 <mayurimoreofficial19@gmail.com>
2025-10-15 17:22:10 -0500

    hollow pyramid

---
+++ hollow-pyramid-pattern.jsx
@@ -0,0 +1,18 @@
+let pattern = "";
+let num = 5;
+
+for (let i = 1; i <= num; i++) {
+  for (let j = 1; j <= num - i; j++) {
+    pattern += "  ";
+  }
+
+  for (let k = 1; k <= 2 * i - 1; k++) {
+    if (k === 1 || k === 2 * i - 1 || i === num) {
+      pattern += "* ";
+    } else {
+      pattern += "  ";
+    }
+  }
+  pattern += "\n";
+}
+console.log(pattern);
